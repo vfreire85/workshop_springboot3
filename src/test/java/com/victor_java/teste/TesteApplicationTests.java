e4686c55a99b390d051abd2b6ac2ea4dbd32a6ac
vfreire85 <victorfreire85@gmail.com>
2024-07-08 11:29:24 -0300

    Project Created

---
+++ src/test/java/com/victor_java/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor_java.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
