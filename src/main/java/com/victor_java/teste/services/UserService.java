98208e48f2e89516035101fa1dfb971afb7a97f2
vfreire85 <victorfreire85@gmail.com>
2024-07-22 09:15:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/victor_java/teste/services/UserService.java
@@ -0,0 +1,26 @@
+package com.victor_java.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victor_java.teste.entities.User;
+import com.victor_java.teste.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
