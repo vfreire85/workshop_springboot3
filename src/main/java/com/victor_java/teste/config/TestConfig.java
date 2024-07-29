f2f0362bc295189ea38fa67a4a24f6375aea07a0
vfreire85 <victorfreire85@gmail.com>
2024-07-29 08:22:19 -0300

    Payment entity

--- src/main/java/com/victor_java/teste/config/TestConfig.java
+++ src/main/java/com/victor_java/teste/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.victor_java.teste.entities.Category;
 import com.victor_java.teste.entities.Order;
 import com.victor_java.teste.entities.OrderItem;
+import com.victor_java.teste.entities.Payment;
 import com.victor_java.teste.entities.Product;
 import com.victor_java.teste.entities.User;
 import com.victor_java.teste.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
